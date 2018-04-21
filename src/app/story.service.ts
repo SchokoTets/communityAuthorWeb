13edeea44a12ef6523221485b2cb12e8aa562cb0
SchokoTets <moritzleni@gmail.com>
2018-04-21 23:43:00 +0200

    improved sendWord() in the story.service

--- src/app/story.service.ts
+++ src/app/story.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class StoryService {
-
+  ip: string = "http://127.0.0.1:8081";
   constructor(private http: HttpClient) { }
 
   escapeDQ(str: string): string {
@@ -14,13 +14,13 @@ export class StoryService {
   }
 
   getStory(): Observable<string> {
-    return this.http.get("http://127.0.0.1:8081/story", { responseType: 'text'});
+    return this.http.get(this.ip + "/story", { responseType: 'text'});
   }
 
-  sendWord(word: string): Observable<string> {
-    //return this.http.get<string>("http://127.0.0.1:8081/test", { responseType: 'text'});
+  sendWord(word: string): void {
     const options = { headers: new HttpHeaders({ 'Content-Type':  'application/json' }), responseType: 'text' as 'text' };
-    return this.http.post("http://127.0.0.1:8081/submit", '{"word": "' + this.escapeDQ(word) + '"}', options );
-    //return of(word + " ");
+    console.log("Trying to send: "+ word);
+    const body = {word: this.escapeDQ(word), uuid: String(Math.floor(Math.random() * 1000))};
+    this.http.post(this.ip + "/submit", JSON.stringify(body), options ).subscribe(response => console.log("Server responded: " + response));
   }
 }

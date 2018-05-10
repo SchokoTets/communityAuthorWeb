e2c5daa409e7156cc2816637cccc2b7a041a0b1a
SchokoTets <moritzleni@gmail.com>
2018-05-10 19:30:46 +0200

    moved IP config to environment file

--- src/app/story.service.ts
+++ src/app/story.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class StoryService {
-  ip: string = "http://127.0.0.1:8081";
+  ip: string = environment.apiEndpoint;
   uuid: number = Math.floor(Math.random() * 1000);
 
   constructor(private http: HttpClient) { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'http://85.14.242.210:8081'
 };
